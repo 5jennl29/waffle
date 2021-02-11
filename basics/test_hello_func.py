03cff7c680008b24a2bb5e608491b3984d7e1ae5
Louise Jennings <5jennl29@solent.ac.uk>
2021-02-11 16:02:11 +0000

    Added test files to basics (testing functions)

---
+++ basics/test_hello_func.py
@@ -0,0 +1,8 @@
+def hello(): #this def statement defines the function named hello
+    print('Howdy!')  #this is the body of the function, what you want the function to do
+    print('Howdy!!!')
+    print('Hello there!')
+
+hello() #this is a function call. It calls for the function to execute
+hello()
+hello()

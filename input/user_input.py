13ccc3136a2fc315e325320d74104bd6ca984483
Louise Jennings <5jennl29@solent.ac.uk>
2021-02-18 10:54:40 +0000

    Added file to take user name and age, and return the year in which they will be 100 years old.

---
+++ input/user_input.py
@@ -0,0 +1,11 @@
+print("\nWhat is your name?")
+name = input()
+print(f"\nHow old are you {name}?")
+age = int(input())
+
+year = 2021
+100 - age = years_to_go
+years_to_go + 2021 = cent_age
+
+print(f"Hi {name}, you will be 100 years old in {cent_age}")
+

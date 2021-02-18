3e47ac5584c917a44377a0119431cb3aa34be84f
Louise Jennings <5jennl29@solent.ac.uk>
2021-02-18 11:00:33 +0000

    Fixed errors on previous file user_input and added repeat message

--- input/user_input.py
+++ input/user_input.py
@@ -4,8 +4,11 @@ print(f"\nHow old are you {name}?")
 age = int(input())
 
 year = 2021
-100 - age = years_to_go
-years_to_go + 2021 = cent_age
+years_to_go = 100 - age
+cent_age = years_to_go + 2021
 
-print(f"Hi {name}, you will be 100 years old in {cent_age}")
+print(f"Hi {name}, you will be 100 years old in {cent_age}\n")
+print("Please enter a number:")
+number = int(input())
 
+print(f"Hi {name}, you will be 100 years old in {cent_age}\n" * number)
